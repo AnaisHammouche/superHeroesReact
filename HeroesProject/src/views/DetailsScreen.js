e24caee05e5262c74f952f38d25ee9541280b0f8
Barthemymickael <72488225+Barthemymickael@users.noreply.github.com>
2023-01-05 22:13:27 +0100

    Ajout du css

--- HeroesProject/src/views/DetailsScreen.js
+++ HeroesProject/src/views/DetailsScreen.js
@@ -1,11 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  View,
-  StyleSheet,
-  Image,
-  Text,
-} from 'react-native';
+import {SafeAreaView, View, StyleSheet, Image, Text} from 'react-native';
 import api from '../api/api';
 
 const LogIn = () => {
@@ -25,21 +19,30 @@ const LogIn = () => {
 
   return (
     <SafeAreaView style={styles.screen}>
-      <View style={styles.container}>
+      <View style={styles.container1}>
         <Image />
-        <Text>Biographie du héro</Text>
         <Image
           source={{
             uri: data2.url,
           }}
-          style={{width: 100, height: 100}}
-          resizeMode={'cover'}></Image>
-        <Text>Nom : {data['name'] + ''}</Text>
-        <Text>Alter ego : {data['alter-egos'] + ''}</Text>
-        <Text>Pseudonyme(s) : {data['aliases'] + ''}</Text>
-        <Text>Lieu de naissance : {data['place-of-birth'] + ''}</Text>
-        <Text>Première apparition : {data['first-appearance'] + ''}</Text>
-        <Text>Publié par : {data['publisher'] + ''}</Text>
+          style={styles.image}
+          resizeMode={'cover'}
+        />
+      </View>
+      <View style={styles.container2}>
+        <Text style={styles.nom}>{data.name + ''}</Text>
+        <Text style={styles.title}>Biographie du héro</Text>
+        <Text style={styles.ego}>Alter ego : {data['alter-egos'] + ''}</Text>
+        <Text style={styles.psd}>Pseudonyme(s) : {data.aliases + ''}</Text>
+        <Text style={styles.date}>
+          Lieu de naissance : {data['place-of-birth'] + ''}
+        </Text>
+        <Text style={styles.apparition}>
+          Premiere apparition : {data['first-appearance'] + ''}
+        </Text>
+        <Text style={styles.publication}>
+          Publié par : {data.publisher + ''}
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -49,12 +52,66 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
-  container: {
+  container1: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '10%',
+  },
+  container2: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-evenly',
-    alignItems: 'center',
+    alignItems: 'right',
     marginBottom: '30%',
+    marginLeft: '5%',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',},
+  image: {
+    width: 170,
+    height: 170,
+    borderRadius: 100,
+    borderWidth: 5,
+    borderColor: '#fff',
+  },
+  nom: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  ego: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',
+  },
+  psd: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',
+  },
+  date: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',
+  },
+  apparition: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',
+  },
+  publication: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: 'grey',
   },
 });
 

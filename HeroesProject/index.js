7b15ef86915e635e985d9af219bc3601bbdc1a64
Sooniii <theodulejanchen@hotmail.fr>
2023-01-05 19:19:59 +0100

    detail hero
    detail hero sans le css

--- HeroesProject/index.js
+++ HeroesProject/index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import LogIn from './src/views/LogIn';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LogIn);
+AppRegistry.registerComponent(appName, () => App);
